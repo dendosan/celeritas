7cc7585a0ed44ece12b007945231ad1fa934af2e
Denton Elliott <dendo74@hotmail.com>
2022-05-29 13:00:26 -0400

    structured tests and more coverage

--- render/render_test.go
+++ render/render_test.go
@@ -6,13 +6,52 @@ import (
 	"testing"
 )
 
+var pageData = []struct {
+	name          string
+	renderer      string
+	template      string
+	errorExpected bool
+	errorMessage  string
+}{
+	{"go_page", "go", "home", false, "error rendering go template"},
+	{"go_page_no_template", "go", "no-file", true, "no error rendering non-existent go template, when one is expected"},
+	{"jet_page", "jet", "home", false, "error rendering jet template"},
+	{"jet_page_no_template", "jet", "no-file", true, "no error rendering non-existent jet template, when one is expected"},
+	{"invalid_render_engine", "bad", "home", true, "no error rendering with non-existent template engine"},
+}
+
 func TestRender_Page(t *testing.T) {
-	r, err := http.NewRequest(("GET"), "/some-url", nil)
+
+	for _, test := range pageData {
+		r, err := http.NewRequest(("GET"), "/some-url", nil)
+		if err != nil {
+			t.Error(err)
+		}
+
+		w := httptest.NewRecorder()
+
+		testRenderer.Renderer = test.renderer
+		testRenderer.RootPath = "./testdata"
+
+		err = testRenderer.Page(w, r, test.template, nil, nil)
+		if test.errorExpected {
+			if err == nil {
+				t.Errorf("%s: %s", test.name, test.errorMessage)
+			}
+		} else {
+			if err != nil {
+				t.Errorf("%s: %s: %s", test.name, test.errorMessage, err.Error())
+			}
+		}
+	}
+}
+
+func TestRender_GoPage(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest("GET", "/url", nil)
 	if err != nil {
 		t.Error(err)
 	}
-
-	w := httptest.NewRecorder()
 
 	testRenderer.Renderer = "go"
 	testRenderer.RootPath = "./testdata"
@@ -20,6 +59,14 @@ func TestRender_Page(t *testing.T) {
 	err = testRenderer.Page(w, r, "home", nil, nil)
 	if err != nil {
 		t.Error("Error rendering page", err)
+	}
+}
+
+func TestRender_JetPage(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest("GET", "/url", nil)
+	if err != nil {
+		t.Error(err)
 	}
 
 	testRenderer.Renderer = "jet"
